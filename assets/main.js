6bffba8e069c730ea0e93c7b161b069665565dc1
Priscilla Trejo <s.priscilla.trejo@gmail.com>
2024-09-04 10:27:52 -0400

    Fixed/Added Dog API

--- assets/main.js
+++ assets/main.js
@@ -13,75 +13,72 @@ function getCatFact() {
         })
         .catch(error => console.error('Error fetching the cat picture:', error));
 
-    const buttonTexts = ["Show Me a Cat!", "More Cats, Please!", "Next Cat!", "Give Me a Cat!", "Here, kitty-kitty!", "Meow?", "Meowza!"];
-    document.querySelector("button").textContent = buttonTexts[Math.floor(Math.random() * buttonTexts.length)];
+    const catButtonTexts = ["Show Me a Cat!", "More Cats, Please!", "Next Cat!", "Give Me a Cat!", "Here, kitty-kitty!", "Meow?", "Meowza!"];
+    document.querySelector(".cat-button").textContent = catButtonTexts[Math.floor(Math.random() * catButtonTexts.length)];
+
+    const catAudioClips = [
+        'assets/audio/kitten-meow.mp3',
+        'assets/audio/cat-purr-meow.mp3',
+        'assets/audio/cat-whine.wav'
+    ];
+    
+    function playRandomCatAudio() {
+        const audioElement = document.getElementById('cat-audio');
+        const randomIndex = Math.floor(Math.random() * catAudioClips.length);
+        const randomClip = catAudioClips[randomIndex];
+        audioElement.src = randomClip;
+        audioElement.play();
+    }
 
     playRandomCatAudio();
 }
 
-const catAudioClips = [
-    'assets/audio/kitten-meow.mp3',
-    'assets/audio/cat-purr-meow.mp3',
-    'assets/audio/cat-whine.wav'
-];
+//
 
-function playRandomCatAudio() {
-    const audioElement = document.getElementById('cat-audio');
-    const randomIndex = Math.floor(Math.random() * catAudioClips.length);
-    const randomClip = catAudioClips[randomIndex];
-    audioElement.src = randomClip;
-    audioElement.play();
+function getDogFact() {
+    fetch('https://dogapi.dog/api/v2/facts')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('dog-fact').textContent = data.data[0].attributes.body;
+    })
+    .catch(error => console.error('Ruh-roh! Error fetching the dog fact:', error));
+
+    
+    fetch('https://api.thedogapi.com/v1/images/search/')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('dog-pic').src = data[0].url;
+        })
+        .catch(error => console.error('Ruh-roh! Error fetching the dog picture:', error));
+
+    const dogButtonTexts = ["Fetch Another Fact!", "Next Dog, Please!", "Next Dog!", "Puppy Time!", "Woof! Tell Me More!", "Bork Bork!", "Wow much doge!"];
+    document.querySelector(".dog-button").textContent = dogButtonTexts[Math.floor(Math.random() * dogButtonTexts.length)];
+
+    const dogAudioClips = [
+        'assets/audio/dog-bark1.mp3',
+        'assets/audio/dog-bark2.mp3',
+        'assets/audio/dog-bark3.mp3'
+    ];
+    
+    function playRandomDogAudio() {
+        const audioElement = document.getElementById('dog-audio');
+        const randomIndex = Math.floor(Math.random() * dogAudioClips.length);
+        const randomClip = dogAudioClips[randomIndex];
+        audioElement.src = randomClip;
+        audioElement.play();
+    }
+
+    playRandomDogAudio();
 }
 
-// function getDogFact() {
-//     fetch('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/?number=1')
-//         .then(response => response.json())
-//         .then(data => {
-//             document.getElementById('dog-fact').textContent = data.data[0];
-//         })
-//         .catch(error => console.error('Ruh-roh! Error fetching the dog fact:', error));
-    
-//     fetch('api.thedogapi.com')
-//         .then(response => response.json())
-//         .then(data => {
-//             document.getElementById('dog-pic').src = data[0].url;
-//         })
-//         .catch(error => console.error('Ruh-roh! Error fetching the dog picture:', error));
+function showCatContent() {
+    document.getElementById('cat-container').classList.remove('hidden');
+    document.getElementById('dog-container').classList.add('hidden');
+    getCatFact();
+}
 
-//     const buttonTexts = ["Show Me a Cat!", "More Cats, Please!", "Next Cat!", "Give Me a Cat!", "Here, kitty-kitty!", "Meow?", "Meowza!"];
-//     document.querySelector("button").textContent = buttonTexts[Math.floor(Math.random() * buttonTexts.length)];
-
-//     playRandomDogAudio();
-// }
-
-// const dogAudioClips = [
-//     'assets/audio/dog-bark1.mp3',
-//     'assets/audio/dog-bark2.mp3',
-//     'assets/audio/dog-bark3.wav'
-// ];
-
-// function playRandomDogAudio() {
-//     const audioElement = document.getElementById('dog-audio');
-//     const randomIndex = Math.floor(Math.random() * dogAudioClips.length);
-//     const randomClip = dogAudioClips[randomIndex];
-//     audioElement.src = randomClip;
-//     audioElement.play();
-// }
-
-
-// function switchAnimal() {
-//     const titleElement = document.getElementById('dog-title');
-//     const imageElement = document.getElementById('dog-image');
-//     const factElement = document.getElementById('dog-fact');
-
-
-//     titleElement.textContent = "Random dog fact:";
-
-
-//     const randomFactIndex = getRandomIndex(catFacts);
-//     factElement.textContent = catFacts[randomFactIndex];
-
-
-//     const randomImageIndex = getRandomIndex(catImages);
-//     imageElement.src = catImages[randomImageIndex];
-// }
+function showDogContent() {
+    document.getElementById('dog-container').classList.remove('hidden');content
+    document.getElementById('cat-container').classList.add('hidden');content
+    getDogFact();
+}
